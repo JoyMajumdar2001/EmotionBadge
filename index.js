7780c85878094e4d06d8c63bb5c502a0052f67e2
JoyMajumdar2001 <joymajumdar15828@gmail.com>
2022-02-23 19:21:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,62 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const passport = require("passport");
 const axios = require("axios").default;
-const GitHubStrategy = require("passport-github").Strategy;
-const session = require("express-session");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 2 * 24 * 60 * 60 * 1000,
-    },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function (user, cb) {
-  cb(null, user);
-});
-
-passport.use(
-  new GitHubStrategy(
-    {
-      clientID: "null",
-      clientSecret: "null",
-      callbackURL: "http://localhost:3000/auth/github/callback",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      cb(null, profile);
-    }
-  )
-);
-
-app.get("/auth/github", passport.authenticate("github"));
-
-app.get(
-  "/auth/github/callback",
-  passport.authenticate("github", { failureRedirect: "/login" }),
-  function (req, res) {
-    res.redirect("/");
-  }
-);
 
 app.get("/emotion/:gitId/", function (req, res) {
   axios.get('https://api.github.com/users/' + req.params.gitId)
@@ -86,95 +35,41 @@ app.get("/emotion/:gitId/", function (req, res) {
 
 
 app.get("/emotion/:gitId/:emotion", function (req, res) {
+  var linkRedir = ""
   if(req.params.emotion == 'smile'){
-    res.send("Hii")
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Smile&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
   }
+  if(req.params.emotion == 'anger'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Anger&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+  if(req.params.emotion == 'contempt'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Contempt&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+  if(req.params.emotion == 'disgust'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Disgust&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+
+  if(req.params.emotion == 'fear'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Fear&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+  if(req.params.emotion == 'happiness'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Happiness&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+  if(req.params.emotion == 'neutral'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Neutral&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+  if(req.params.emotion == 'sadness'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Sadness&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+  if(req.params.emotion == 'surprise'){
+    linkRedir = "https://img.shields.io/badge/dynamic/json?color=orange&label=Surprise&query=%24.faceAttributes.smile&url=https%3A%2F%2Femotionbadge.onrender.com%2Femotion%2F" + req.params.gitId
+  }
+
+  res.redirect(linkRedir);
+  
 
 });
 
-const isAuth = (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.redirect("/login");
-  }
-};
-
-app.get("/", isAuth, (req, res) => {
-  var data = JSON.stringify({
-    url: req.user._json.avatar_url.slice(0, -4),
-  });
-
-  var config = {
-    method: "post",
-    url: "https://faceapigit.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=emotion,smile&recognitionModel=recognition_01&returnRecognitionModel=false&detectionModel=detection_01&faceIdTimeToLive=86400",
-    headers: {
-      "Ocp-Apim-Subscription-Key": "d3015b46246f4d9498f6cc4817f12fdc",
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
-
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      var resJson = response.data;
-
-      if (resJson.length == 0) {
-        let params = {
-          name: req.user.displayName,
-          dp: req.user._json.avatar_url,
-          dataStr: JSON.stringify(response.data),
-          hasFase: false,
-        };
-        res.render("dashboard", params);
-      } else {
-        let smile = Math.round(resJson[0].faceAttributes.smile * 100) / 100;
-        let anger =
-          Math.round(resJson[0].faceAttributes.emotion.anger * 100) / 100;
-        let fear =
-          Math.round(resJson[0].faceAttributes.emotion.fear * 100) / 100;
-        let sadness =
-          Math.round(resJson[0].faceAttributes.emotion.sadness * 100) / 100;
-        let neutral =
-          Math.round(resJson[0].faceAttributes.emotion.neutral * 100) / 100;
-        let disgust =
-          Math.round(resJson[0].faceAttributes.emotion.disgust * 100) / 100;
-        let surprise =
-          Math.round(resJson[0].faceAttributes.emotion.surprise * 100) / 100;
-
-        let params = {
-          name: req.user.displayName,
-          dp: req.user._json.avatar_url,
-          smile: smile,
-          anger: anger,
-          fear: fear,
-          sadness: sadness,
-          neutral: neutral,
-          disgust: disgust,
-          surprise: surprise,
-          hasFase: true,
-        };
-        res.render("dashboard", params);
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-      let params = {
-        name: req.user.displayName,
-        dp: req.user._json.avatar_url,
-        dataStr: "error",
-      };
-      res.render("dashboard", params);
-    });
-});
-
-app.get("/login", (req, res) => {
-  if (req.user) {
-    return res.redirect("/");
-  }
-  res.render("login");
-});
 
 app.set("view engine", "ejs");
 
